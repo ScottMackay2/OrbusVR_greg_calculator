dd703ebbf79df8c5af6a1e5cd3837da7133d1ca2
Scott <scottmackay@live.nl>
2021-11-11 21:05:01 +0100

    Added mage delay from affliction to more realistically make best possible tilesets.

--- allClassData.js
+++ allClassData.js
@@ -129,6 +129,8 @@ function getMageData(){
 	const FIRE_CAST_SPEED = 0.43;
 	const FROST_CAST_SPEED = 0.43;
 	const AFFLICTION_CAST_SPEED = 0.55;
+	const AFFLICTION_TRAVEL_SPEED = AFFLICTION_CAST_SPEED + 0.2;
+	const FROST_CAST_SPEED_AFTER = 0.43 - (AFFLICTION_TRAVEL_SPEED-AFFLICTION_CAST_SPEED);
 	var mageData = {
 		attackTypes : {
 			// Default boost of class
@@ -143,7 +145,7 @@ function getMageData(){
 			'f'    : mage.getAttackFromInfo({type:"Frost2",time:FROST_CAST_SPEED,empowered:true}),
 			// 'f'    : new Attack(0.43,		9460*AFFINITY_FIRE_BOOST_MAGE*NORMAL_BOOST_MAGE, true,	0, 0, 0,			0.00, 0, 0, 	1, 0,"F",""),
 			// Frost boosted by runic diversity (3th spell)
-			'F'    : mage.getAttackFromInfo({type:"Frost2",time:FROST_CAST_SPEED,diversity:true}),
+			'F'    : mage.getAttackFromInfo({type:"Frost2",time:FROST_CAST_SPEED_AFTER,diversity:true}),
 			// 'F'    : new Attack(0.43,		9460*RUNIC_DIVERSITY_MAGE*NORMAL_BOOST_MAGE, true,		0, 0, 0,			0.00, 0, 0, 	1, 0,"F",""),
 			// Frost boosted by runic diversity and fireball.
 			'V'    : mage.getAttackFromInfo({type:"Frost2",time:FROST_CAST_SPEED,empowered:true,diversity:true}),
@@ -151,9 +153,9 @@ function getMageData(){
 
 			// Affliction affinity fireball
 			// 'a'    : new Attack(0.55,		3542*AFFINITY_FIRE_BOOST_MAGE*NORMAL_BOOST_MAGE, true,	3442, 12, 3,		0.05, 8, 2, 	1, 1,"A",""),
-			'a'    : mage.getAttackFromInfo({type:"Affliction2",time:AFFLICTION_CAST_SPEED,empowered:true}),
+			'a'    : mage.getAttackFromInfo({type:"Affliction2",time:AFFLICTION_TRAVEL_SPEED,empowered:true}),
 			// Affliction runic diversity and frost boosted
-			'A'    : mage.getAttackFromInfo({type:"Affliction2",time:AFFLICTION_CAST_SPEED,diversity:true,dotIncrease:true}),
+			'A'    : mage.getAttackFromInfo({type:"Affliction2",time:AFFLICTION_TRAVEL_SPEED,diversity:true,dotIncrease:true}),
 			// 'A'    : new Attack(0.55,		3542*RUNIC_DIVERSITY_MAGE*NORMAL_BOOST_MAGE, true,		3442, 15, 3,		0.05, 8, 2, 	1, 1,"A",""),
 			// Renew (constant heal tile)
 			'h'    : new Attack(0.0,		0, true,	0, 20, 1,	0.00, 0, 0, 	0, 0,"","H"),
