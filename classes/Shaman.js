d6909cb1ea959b93d732b954a39eee3709f59327
Scott <scottmackay@live.nl>
2022-06-02 13:37:27 +0200

    Small tweak for shaman dps behavior.

--- classes/Shaman.js
+++ classes/Shaman.js
@@ -30,7 +30,7 @@ define(["./Attack"], function (Attack) {
 				}
 				if(attackData.type === "Frost"){
 					usesProjectile = false;
-					damage = baseMult*0.372687;
+					damage = baseMult*0.372712;
 					if(this.dotIncrease !== undefined){
 						damage *= (1+this.dotIncrease);
 					}
@@ -40,7 +40,7 @@ define(["./Attack"], function (Attack) {
 				}
 				if(attackData.type === "Lava"){
 					usesProjectile = false;
-					damage = baseMult*2.01258;
+					damage = baseMult*2.01265;
 					if(this.dotIncrease !== undefined){
 						damage *= (1+this.dotIncrease);
 					}
@@ -49,11 +49,12 @@ define(["./Attack"], function (Attack) {
 					tiles = "P";
 				}
 				if(attackData.type === "Stun"){
-					// Increased Stun 1.4909
-					damage = baseMult*0.6595 * (1+0.1725*WEAKNESS_COUNT);
+					damage = baseMult*0.3727158;
+					const addedWeaknessDamage = damage*(0.69*WEAKNESS_COUNT);
 					if(this.talentlvl5 == "Stunning"){
-						damage*=2.26;
+						damage += damage*3;
 					}
+					damage += addedWeaknessDamage;
 
 					// 30 (2 weakness) = 8416; 
 					// 30+6 (no weakness) = 8816
@@ -62,7 +63,7 @@ define(["./Attack"], function (Attack) {
 					tiles = "S";
 				}
 				if(attackData.type === "Lightning"){
-					damage = baseMult*2.23625 * (1+0.5750*WEAKNESS_COUNT);
+					damage = baseMult*2.2362947 * (1+0.5750*WEAKNESS_COUNT);
 					// 30 (2 weakness) = 20180; 
 					// 30+6 (no weakness) = 13224
 					// 30+6 (1 weakness) = 20828
@@ -74,14 +75,14 @@ define(["./Attack"], function (Attack) {
 					attackID = 2;
 				}
 				if(attackData.type === "Fire"){
-					damage = baseMult*2.9817 * (1+0.8625*WEAKNESS_COUNT); // 689.346
+					damage = baseMult*2.9817263 * (1+0.8625*WEAKNESS_COUNT); // 689.346
 					// 30 (2 weakness) = 34103; 
 					// 30+6 (no weakness) = 17632
 					// 30+6 (1 weakness) = 32840
 					// 30+6 (2 weakness) = 48047
 					tiles = "B";
 					if(this.talentlvl10 === "Fire Consumes"){
-						dotDamage = baseMult*0.559;
+						dotDamage = baseMult*0.559075;
 						if(this.dotIncrease !== undefined){
 							dotDamage *= (1+this.dotIncrease);
 						}
